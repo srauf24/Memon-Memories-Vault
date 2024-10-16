cd92c03bc8a7dd27794a57c1f1b128c129dd3627
Samee Rauf <samee.rauf@pmg.com>
2024-10-16 15:53:50 -0700

    installed prettier package for formatting

--- memon-voice-vault/app/page.js
+++ memon-voice-vault/app/page.js
@@ -1,61 +1,64 @@
-'use client';
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import {
   Box,
   Modal,
-    Fade,
+  Fade,
   Stack,
   TextField,
   Typography,
   Button,
   Card,
   CardContent,
-} from '@mui/material';
-import dynamic from 'next/dynamic';
-import ThumbUp from '@mui/icons-material/ThumbUp';
-import { Remove } from '@mui/icons-material';
+} from "@mui/material";
+import dynamic from "next/dynamic";
+import ThumbUp from "@mui/icons-material/ThumbUp";
+import { Remove } from "@mui/icons-material";
 const MemonMemoryVault = () => {
-    const [open, setOpen] = useState(false); // Initially, modal is closed
-     const handleOpen = () => setOpen(true);
-     const handleClose = () => setOpen(false);
-
+  const [open, setOpen] = useState(false); // Initially, modal is closed
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
-      <Box
-          width="100vw"
-          height="100vh"
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          gap={2}>
-        <h3> Memon Memories Vault</h3>
-      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description" >
-        <Fade in={open} >
-        <Box position="absolute"
-          top="50%"
-          left="50%"
-          transform="translate(-50%,-50%)"
-          width={{ xs: '90%', sm: '400px' }} // Responsive width
-          bgcolor="white"
-          border={'2px solid #000'}
-          boxShadow={24}
-          p={4}
-          display="flex"
-          flexDirection="column"
-          gap={3}>
-          <Typography variant="h5">Add a New Quote</Typography>{' '}
-
-        </Box>
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+    >g
+      <h3> Memon Memories Vault</h3>
+      <Button variant="outlined">
+        <Typography variant="button">Add a new memory</Typography>{" "}
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Fade in={open}>
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%,-50%)"
+            width={{ xs: "90%", sm: "400px" }} // Responsive width
+            bgcolor="white"
+            border={"2px solid #000"}
+            boxShadow={24}
+            p={4}
+            display="flex"
+            flexDirection="column"
+            gap={3}
+          ></Box>
         </Fade>
-
       </Modal>
-      </Box>
-
-
+    </Box>
   );
-}
+};
 export default dynamic(() => Promise.resolve(MemonMemoryVault), { ssr: false });
