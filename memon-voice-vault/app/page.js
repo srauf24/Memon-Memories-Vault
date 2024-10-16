3e73361f7f0ffd1393e46da59659b7fd94b3e1f5
Samee Rauf <samee.rauf@pmg.com>
2024-10-16 15:44:34 -0700

    added fade

--- memon-voice-vault/app/page.js
+++ memon-voice-vault/app/page.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import {
   Box,
   Modal,
+    Fade,
   Stack,
   TextField,
   Typography,
@@ -15,7 +16,12 @@ import {
 import dynamic from 'next/dynamic';
 import ThumbUp from '@mui/icons-material/ThumbUp';
 import { Remove } from '@mui/icons-material';
-const memonMemoryVault = () => {
+const MemonMemoryVault = () => {
+    const [open, setOpen] = useState(false); // Initially, modal is closed
+     const handleOpen = () => setOpen(true);
+     const handleClose = () => setOpen(false);
+
+
   return (
       <Box
           width="100vw"
@@ -26,8 +32,30 @@ const memonMemoryVault = () => {
           alignItems="center"
           gap={2}>
         <h3> Memon Memories Vault</h3>
+      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description" >
+        <Fade in={open} >
+        <Box position="absolute"
+          top="50%"
+          left="50%"
+          transform="translate(-50%,-50%)"
+          width={{ xs: '90%', sm: '400px' }} // Responsive width
+          bgcolor="white"
+          border={'2px solid #000'}
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}>
+          <Typography variant="h5">Add a New Quote</Typography>{' '}
+
+        </Box>
+        </Fade>
+
+      </Modal>
       </Box>
+
 
   );
 }
-export default dynamic(() => Promise.resolve(memonMemoryVault), { ssr: false });
+export default dynamic(() => Promise.resolve(MemonMemoryVault), { ssr: false });
